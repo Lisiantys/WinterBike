816bf68f88c398ff499eea298a0a56b5bb8989d8
Lisiantys <maxime.pouzargue7@gmail.com>
2023-05-02 21:40:48 +0200

    V0.1 - Model/Migrations OK - Breeze OK

---
+++ database/migrations/2023_05_02_185336_create_events_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image_path');
+            $table->timestamp('beginningDate')->useCurrent();
+            $table->timestamp('endDate')->nullable();
+            $table->string('address');
+            $table->string('email');
+            $table->string('phone')->nullable()->default(null);
+            $table->string('website')->nullable()->default(null);
+            $table->string('facebook')->nullable()->default(null);
+            $table->mediumText('description');
+            $table->boolean('is_promoted');
+            $table->boolean('is_validated');
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamp('created_at')->nullable();
+            $table->unsignedTinyInteger('department_id');
+            $table->foreign('department_id')->references('id')->on('departments');
+            $table->unsignedTinyInteger('region_id');
+            $table->foreign('region_id')->references('id')->on('regions');
+            $table->unsignedTinyInteger('type_id');
+            $table->foreign('type_id')->references('id')->on('types');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
---
+++ database/migrations/2023_05_02_185540_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->mediumText('description');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('events_id');
+            $table->foreign('events_id')->references('id')->on('events');
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
