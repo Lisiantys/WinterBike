3702923514e5dbe608fd96a1720307f2b5b3ef11
Maxime <98598137+Lisiantys@users.noreply.github.com>
2023-05-11 11:05:32 +0200

    V0.82 - Intégration Laravel Breeze [Partie 1]

--- resources/views/events/partials/navbar.blade.php
+++ resources/views/events/partials/navbar.blade.php
@@ -1,23 +1,32 @@
 <header>
     <nav>
-        
-        @auth
-        <p>Bonjour : {{ auth()->user()->name }}</p>
-            <ul>
-                <li><a href="{{ route('index') }}">Évènement</a></li>
-                <li><a href="{{ route('events.myEvents') }}">Mes Évènements</a></li>
-                @if(auth()->user()->role_id === 3 || auth()->user()->role_id === 4)
-                    <li><a href="{{ route('events.pending') }}">Évènements en attente</a></li>
-                @endif
-                @if(auth()->user()->role_id === 4)
-                    <li><a href="{{ route('profile.manage') }}">Gérer les Utilisateurs</a></li>
-                @endif
-            </ul>
-        @endauth
-        @guest
-            <ul>
-                <li><a href="{{ route('index') }}">Évènement</a></li>
-            </ul>
-        @endguest
+        @if (Route::has('login'))
+            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+                @auth
+                    <a href="{{ url('/dashboard') }}">Dashboard</a>
+                    <p>Bonjour : {{ auth()->user()->name }}</p>
+                <ul>
+                    <li><a href="{{ route('index') }}">Évènement</a></li>
+                    <li><a href="{{ route('events.myEvents') }}">Mes Évènements</a></li>
+                    @if(auth()->user()->role_id === 3 || auth()->user()->role_id === 4)
+                        <li><a href="{{ route('events.pending') }}">Évènements en attente</a></li>
+                    @endif
+                    @if(auth()->user()->role_id === 4)
+                        <li><a href="{{ route('profile.manage') }}">Gérer les Utilisateurs</a></li>
+                    @endif
+                </ul>
+                @endauth
+                @guest
+                <ul>
+                    <li><a href="{{ route('index') }}">Évènement</a></li>
+                </ul>
+                    <a href="{{ route('login') }}">Log in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Register</a>
+                    @endif
+                @endguest
+            </div>
+        @endif
     </nav>
 </header>
