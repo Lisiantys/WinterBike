3f03b9aa94be132f2e3493957faf45efcc6fd5d7
Lisiantys <maxime.pouzargue7@gmail.com>
2023-05-25 00:00:47 +0200

    V0.98.2 - Page Show Design

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -1,62 +1,67 @@
 <x-app-layout>
-    <h1>{{ $event->name }}</h1>
-    <p>{{ $event->description }}</p>
-    <a href="{{ route('profile.show', $event->user->id) }}">{{ $event->user->name }}</a>
     <img id="image-preview" src="{{ Storage::url($event->image_path) }}" alt="Aperçu de l'image" style="max-width: 100%;">
-   
-    @if (auth()->user() && $event->favoritedBy->contains(auth()->user()->id))
-    <p><i class="fa-solid fa-star fa-xl" style="color: #FFD700;">{{ $event->favoritedBy->count() }}</i></p>
-    @else
-        <p><i class="fa-regular fa-star fa-xl" style="color: #e7ca25;">{{ $event->favoritedBy->count() }}</i></p>
-    @endif
-    <!-- AddToAny BEGIN -->
-    <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
-        <a class="a2a_button_facebook"></a>
-        <a class="a2a_button_twitter"></a>
-        <a class="a2a_button_skype"></a>
+    <x-h1-title>
+        {{ $event->name }}
+    </x-h1-title>
+    <div class="flex justify-between items-center">
+        <p class="text-gray-500">Du <strong>{{ \Carbon\Carbon::parse($event->beginningDate)->format('d/m/Y') }}</strong> au <strong>{{ \Carbon\Carbon::parse($event->endDate)->format('d/m/Y') }}</strong> - {{ $event->type->name }}</p>
+        @if (auth()->user() && $event->favoritedBy->contains(auth()->user()->id))
+            <p><i class="fa-solid fa-star fa-xl" style="color: #FFD700;">{{ $event->favoritedBy->count() }}</i></p>
+        @else
+            <p><i class="fa-regular fa-star fa-xl" style="color: #e7ca25;">{{ $event->favoritedBy->count() }}</i></p>
+        @endif
+    </div>
+    <p class="mt-2 text-gray-500">{{ $event->region->name }} - {{ $event->department->name }}</p>
+    <p>{{ $event->description }}</p>
+    <div class="flex items-center">
+        <img class="h-10 w-10 rounded-full" src="{{ Storage::url($event->user->image_path) }}" alt="Image de l'utilisateur">
+        <a href="{{ route('profile.show', $event->user->id) }}" class="ml-2">{{ $event->user->name }}</a>
+    </div>
+    <div class="flex items-center">
+        <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
+            <a class="a2a_button_facebook"></a>
+            <a class="a2a_button_twitter"></a>
+            <a class="a2a_button_skype"></a>
+        </div>
         <a href="#" id="copyLink"><i class="fa-solid fa-share-nodes fa-xl" style="color: #808080;"></i></a>
     </div>
-    <script>
-    var a2a_config = a2a_config || {};
-    a2a_config.locale = "fr";
-    a2a_config.num_services = 4;
-    </script>
-    <script async src="https://static.addtoany.com/menu/page.js"></script>
-    <!-- AddToAny END -->
-  
+    <div class="flex items-center justify-end">
+        @auth
+            @if(auth()->user()->email_verified_at !== null) 
+                @php
+                $isFavorite = auth()->user()->favoritedEvents->contains($event->id);
+                @endphp
 
-    @auth
-        @if(auth()->user()->email_verified_at !== null) 
-            @php
-            $isFavorite = auth()->user()->favoritedEvents->contains($event->id);
-            @endphp
+                @if($isFavorite)
+                    <form action="{{ route('favorites.remove', $event->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="h-10 font-semibold bg-gradient-to-r from-blue-500 to-green-500 text-white py-2 px-4 rounded text-base">Retirer des favoris</button>
+                    </form>
+                @else
+                    <form action="{{ route('favorites.add', $event->id) }}" method="POST">
+                        @csrf
+                        <button type="submit" class="h-10 font-semibold bg-gradient-to-r from-blue-500 to-green-500 text-white py-2 px-4 rounded text-base">Ajouter au favoris</button>
+                    </form>
+                @endif
+            @endif
+        @endauth
 
-            @if($isFavorite)
-                <form action="{{ route('favorites.remove', $event->id) }}" method="POST">
+        @auth
+            @if(auth()->user()->id === $event->user_id || auth()->user()->role_id === 4) {{-- ok --}}
+            <div class="h-10 my-2 w-52 rounded-md bg-gradient-to-r from-blue-500 to-green-500 p-1">
+                <div class="flex h-full  items-center justify-center bg-white">
+                    <a href="{{ route('events.edit', $event->id) }}" class="text-base px-4 font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-500">Modifier l'événement</a>
+                </div>
+            </div>
+                <form action="{{ route('events.destroy', $event->id) }}" method="POST" style="display:inline;">
                     @csrf
                     @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Retirer des favoris</button>
-                </form>
-            @else
-                <form action="{{ route('favorites.add', $event->id) }}" method="POST">
-                    @csrf
-                    <button type="submit" class="btn btn-primary">Ajouter au favoris</button>
+                    <button type="submit" class="h-10 font-semibold bg-gradient-to-r from-red-500 to-orange-500 text-white py-2 px-4 rounded text-base" onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet événement?');">Supprimer l'événement</button>
                 </form>
             @endif
-        @endif
-    @endauth
-
-    @auth
-        @if(auth()->user()->id === $event->user_id || auth()->user()->role_id === 4) {{-- ok --}}
-            <a href="{{ route('events.edit', $event->id) }}" class="btn btn-warning">Modifier l'événement</a>
-
-            <form action="{{ route('events.destroy', $event->id) }}" method="POST" style="display:inline;">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger" onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet événement?');">Supprimer l'événement</button>
-            </form>
-        @endif
-    @endauth
+        @endauth
+    </div>
 
     @auth
         @if(auth()->user()->email_verified_at !== null)  {{-- ok --}}
@@ -69,7 +74,7 @@
                   <label for="comment" class="sr-only">Your comment</label>
                   <textarea id="description" name="description" rows="6"  
                       class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
-                      placeholder="Write a comment..." required></textarea>
+                      placeholder="Écrire un commentaire..." required></textarea>
               </div>
               <button type="submit"
               class="h-10 mt-2 mb-10 font-semibold bg-gradient-to-r from-blue-500 to-green-500 text-white py-2 px-4 rounded text-base">
@@ -137,4 +142,10 @@
             alert('Le lien a été copié dans le presse-papiers !');
         });
     </script>
+    <script>
+        var a2a_config = a2a_config || {};
+        a2a_config.locale = "fr";
+        a2a_config.num_services = 4;
+    </script>
+    <script async src="https://static.addtoany.com/menu/page.js"></script>
 </x-app-layout>
