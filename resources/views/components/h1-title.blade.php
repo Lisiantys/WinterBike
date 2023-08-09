a93cd30ee2ae20086955aa89e89d4b0d3d921c88
Lisiantys <maxime.pouzargue7@gmail.com>
2023-08-09 22:02:41 +0200

    V4.2.2.1 - Ajustement mineur

--- resources/views/components/h1-title.blade.php
+++ resources/views/components/h1-title.blade.php
@@ -3,12 +3,12 @@
         font-family: 'Open Sans', sans-serif;
         text-shadow: 2px 2px #000;
     }
-    .xl-size{
+    .custom-size{
         font-size: clamp(30px, 2.8vw, 65px);
     }
 </style>
 
-<h1 {{ $attributes->merge(['class' => 'text-white font-open-sans xl-size bg-gradient-to-t from-dark-green to-mint w-full px-6 pt-16 mt-20 h-36 lg:mt-0 '])  }}>
+<h1 {{ $attributes->merge(['class' => 'text-white font-open-sans custom-size bg-gradient-to-t from-dark-green to-mint w-full px-6 pt-16 mt-20 h-36 lg:mt-0 '])  }}>
     {{ $slot }}
 </h1>
 <div class="w-full h-16" style="background: linear-gradient(178deg, #10564f 50%, white 50%);"></div>
