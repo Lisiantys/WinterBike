a6481d6993a77fe16cfcf4918d33c3834f5344b4
Maxime <maxime.pouzargue7@gmail.com>
2023-09-04 17:06:37 +0200

    V4.5.3 - Méta Description

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,6 +6,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <meta name="description" content="Rassemblements moto en France, balade, exposition, hivernale, roadtrip, chaque événement se trouve ici."> 
+
 
         <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
         <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
